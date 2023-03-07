63c2ea63c942e7d2594e546466250eb8f52e926e
MinhDuc0510 <minhduc53002@gmail.com>
2023-03-07 20:56:02 +0700

    first commit

---
+++ javascript/index.js
@@ -0,0 +1,78 @@
+const header = document.querySelector("header");
+const getStarted = document.querySelector(".get-started");
+getStarted.style.marginTop = `${header.offsetHeight}px`;
+
+const overlay = document.querySelector(".overlay");
+
+const btnMenu = document.querySelector(".header_icon-menu");
+const listMenuHeader = document.querySelector(".header_menu");
+const btnDeleteMenuHeader = document.querySelector(".header_menu i");
+
+const textCompany = document.querySelector(".company h5");
+const menuCompany = document.querySelector(".footer-list_detail-company");
+
+const textServices = document.querySelector(".services h5");
+const menuServices = document.querySelector(".footer-list_detail-services");
+
+const textContact = document.querySelector(".contact h5");
+const menuContact = document.querySelector(".footer-list_detail-contact");
+
+const btnDeleteMenuFooter = document.querySelectorAll(
+    ".btn-delete_menu-footer"
+);
+
+const handleTop = () => {
+    element.style.top = `${header.offsetHeight}px`;
+};
+
+const handleMenu = (element) => {
+    if (element.classList.contains("appearMenu")) {
+        element.classList.remove("appearMenu");
+        element.classList.add("hiddenMenu");
+        overlay.style.display = "none";
+        setTimeout(() => {
+            element.style.display = "none";
+        }, 400);
+    } else {
+        element.classList.add("appearMenu");
+        element.classList.remove("hiddenMenu");
+        element.style.display = "block";
+        element.style.top = `${header.offsetHeight}px`;
+        overlay.style.display = "block";
+    }
+};
+
+btnMenu.onclick = () => handleMenu(listMenuHeader);
+btnDeleteMenuHeader.onclick = () => handleMenu(listMenuHeader);
+textCompany.onclick = () => handleMenu(menuCompany);
+textServices.onclick = () => handleMenu(menuServices);
+textContact.onclick = () => handleMenu(menuContact);
+
+btnDeleteMenuFooter.forEach((btn) => {
+    btn.onclick = (e) => {
+        let arrElement = [
+            ".footer-list_detail-company",
+            ".footer-list_detail-services",
+            ".footer-list_detail-contact",
+        ];
+        let elementNeedDelete;
+        for (element of arrElement) {
+            if (e.target.closest(element)) {
+                elementNeedDelete = e.target.closest(element);
+                console.log(elementNeedDelete);
+                break;
+            }
+        }
+        handleMenu(elementNeedDelete);
+    };
+});
+
+overlay.onclick = () => {
+    let arrElement = [listMenuHeader, menuCompany, menuServices, menuContact];
+    for (element of arrElement) {
+        if (element.classList.contains("appearMenu")) {
+            handleMenu(element);
+            break;
+        }
+    }
+};
